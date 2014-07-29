9bea0033cd89600f5b4dfadbd647183add533d58
npezza93 <npezza93@gmail.com>
2014-07-28 23:59:33 -0400

    messing around with title image

--- db/migrate/20140720231246_add_avatar_to_users.rb
+++ db/migrate/20140720231246_add_avatar_to_users.rb
@@ -1,5 +1,5 @@
 class AddAvatarToUsers < ActiveRecord::Migration
   def change
-  	add_column :users, :image, :string
+  	add_column :users, :image, :string, default: "../../../../default_pic.png"
   end
 end
