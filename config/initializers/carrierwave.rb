44912aa378f1f3cabce5211f887328bff8839ab7
npezza93 <npezza93@gmail.com>
2014-08-30 16:23:25 -0400

    Delete carrierwave.rb

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAITHT6OY4OVUHCUZQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
-  }
-  config.fog_directory  = 'while_true'                     # required
-end
